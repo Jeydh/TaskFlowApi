8811413f29b2016ecd8862382759b86dea198e40
Jonathan Demey <noctarider@gmail.com>
2026-02-22 16:01:45 +0100

    feat(auth): add protected me endpoint

---
+++ src/Controller/Api/MeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class MeController extends AbstractController
+{
+    #[Route('/api/me', name: 'api_me', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->json([
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        return $this->json([
+            'id' => $user->getId(),
+            'email' => $user->getUserIdentifier(),
+            'roles' => $user->getRoles(),
+        ]);
+    }
+}
